1c7c17b8292942058a2069d51905f18f2ed94a44
Andrew Carvalho <aj.carvalho@andrewcarvalho.com>
2013-08-10 16:26:09 -0400

    Added a crate and a flat shader for low poly, vertex coloured models.

---
+++ UnityProject/Assets/VertexColourizer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VertexColourizer : MonoBehaviour {
+
+    [SerializeField]
+    private Color32 colour;
+
+    private bool set = false;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        Debug.Log("colour is " + this.colour.ToString());
+
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+        Color32[] meshColours = mesh.colors32;
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            meshColours[i] = this.colour;
+        }
+
+        mesh.colors32 = meshColours;
+	}
+
+    // Update is called once per frame
+    void Update() 
+    {
+        //if (!this.set)
+        //{
+        //    Debug.Log("colour is " + this.colour.ToString());
+
+        //    Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        //    for (int i = 0; i < mesh.vertexCount; i++)
+        //    {
+        //        mesh.colors32[i] = this.colour;
+        //    }
+
+        //    this.set = true;
+        //}
+	}
+}

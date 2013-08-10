0acc672abddbbd9c16a5a89e79baa1735008994b
jephrie <championman@hotmail.com>
2013-08-10 16:55:44 -0400

    rotation

---
+++ UnityProject/Assets/RemoveGravity.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchGravity : MonoBehaviour {
+	
+	public string gravityButton;
+	public const float NO_GRAVITY = 0f;
+	public const float NORMAL_GRAVITY = 10f;
+	private CharacterMotor charMove;
+	private bool isGravityOn;
+	
+	
+	// Use this for initialization
+	void Awake () {
+		charMove = gameObject.GetComponent<CharacterMotor>();
+		isGravityOn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown(gravityButton))
+		{
+			if (isGravityOn)
+			{
+				charMove.movement.gravity = NO_GRAVITY;
+			}
+			else
+			{
+				charMove.movement.gravity = NORMAL_GRAVITY;
+			}
+		}
+	}
+}
---
+++ UnityProject/Assets/RotateWorld.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateWorld : MonoBehaviour {
+	
+	public Transform centerOfWorld;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			//Vector3 v = new Vector3();
+			transform.Rotate(Vector3.forward * 90, Space.Self);
+		}
+	}
+}

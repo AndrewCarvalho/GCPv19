9c5e0ca5c7772a18961c6f03c4ee270f83b88771
Jeffrey Tran <championman@hotmail.com>
2013-08-13 20:01:01 -0400

    RotatingQuad is now called Rotate. Rotate can now rotate any rigidbody around it's center, given a speed (direction and angle to rotate)

---
+++ UnityProject/Assets/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    // Default value is 50
+    public Vector3 rotationSpeed;
+    public Rigidbody body;
+
+    private Vector3 rotation;
+
+	void Awake ()
+    {
+        rotation = body.transform.eulerAngles;
+	}
+	
+	void OnTriggerStay ()
+    {
+        rotation += rotationSpeed * Time.deltaTime;
+        body.MoveRotation(Quaternion.Euler(rotation));
+	}
+}

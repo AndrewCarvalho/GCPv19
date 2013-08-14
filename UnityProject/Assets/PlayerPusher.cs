ab18061eae9583e440986031326dee009abd9620
Jeffrey Tran <championman@hotmail.com>
2013-08-13 20:17:13 -0400

    Fix build break. Shouldn't have renamed RotateQuad.

--- UnityProject/Assets/PlayerPusher.cs
+++ UnityProject/Assets/PlayerPusher.cs
@@ -29,7 +29,7 @@ public class PlayerPusher : MonoBehaviour {
 
         if (body.isKinematic)
         {
-            if (body.GetComponent<RotatingQuad>() != null)
+            if (body.GetComponent<Rotate>() != null)
             {
                 Debug.Log("heyheyhey");
 
